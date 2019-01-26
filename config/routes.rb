e5743484ec5d7508871248ce74839c57b2432780
Andrei Dashkevich <andrew.daskhevich@yandex.ru>
2019-01-26 23:19:15 +0300

    remove useless methods, refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api/v1' do
     get '/problems', to: 'problems#index'
-    #get '/charts/problems_count_per_day'
-    get '/charts/problems_by_organization'
-    get '/charts/unsolved_problems_by_organization'
-    get '/charts/problems_by_category'
-    get '/charts/unsolved_problems_by_category'
-    get '/charts/new_problems_per_day'
 
-    get '/charts/problems'
-    get '/charts/users'
-    get '/charts/organizations'
-    get '/charts/categories'
+    scope '/open_data' do
+      get '/problems', to: 'charts#problems'
+      get '/users', to: 'charts#users'
+      get '/organizations', to: 'charts#organizations'
+      get '/categories', to: 'charts#categories'
+    end
   end
 end
